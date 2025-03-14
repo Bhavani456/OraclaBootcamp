ddc2291196f7321913d489cfcb97d5eeab05e830
Bhavani456 <68225705+Bhavani456@users.noreply.github.com>
2025-03-14 12:14:29 +0530

    Update Aggregations.sql

--- SQLPractice/Aggregations.sql
+++ SQLPractice/Aggregations.sql
@@ -107,3 +107,61 @@ group by prodcust_id;
 select cust_id,min(amount_sold) as min_amount_spend
 from sh.SALES
 group by cust_id;
+
+
+
+-- Each product how much sales happens
+select prod_id,count(*) as "num_of_trxns" from sh.sales group by prod_id;
+-- avg amount sold in each product
+select prod_id,avg(amount_sold) as avg_sale from sh.SALES
+group by prod_id;
+-- what is the maximum and minumut amounts getting in each product
+select prod_id,max(amount_sold) as max_sale,min(amount_sold) as min_sale from sh.SALES
+group by prod_id;
+-- it retuens what is total revenue generated from each product
+select prod_id,sum(amount_sold) as Total_revenue from sh.SALES
+group by prod_id  order by Total_revenue;
+-- total no of times customer happens purching each product
+select cust_id,count(*) from sh.SALES
+group by cust_id;
+
+-- It return avg amount spent by each customer
+select cust_id,avg(amount_sold) as avg_spent_amount
+from sh.SALES
+group by cust_id;
+-- it returns maximum product price  purched each customer
+select cust_id,max(amount_sold) as max_amount_spent
+from sh.SALES
+group by prodcust_id;
+-- it returns mimmum product price  purched each customer
+select cust_id,min(amount_sold) as min_amount_spend
+from sh.SALES
+group by cust_id;
+
+--total revenue genarted based customer location
+select cust_city,sum(amount_sold) as Total_revenue from sh.sales s join sh.customers c on s.cust_id=c.CUST_ID
+group by cust_city;
+
+-- It return the total number trnsactions made by each region by joining sales,customer and countries table
+select country_region,count(*) as total_sales_trxns from  sh.sales s join sh.customers c on s.cust_id=c.cust_id
+join sh.countries cr on cr.country_id = c.country_id group by country_region;
+-- It returns total revenue generated from each region  by joingc sales table with  customers table and countries table 
+select country_region,sum(amount_sold) as "Total revenue" from sh.sales s join sh.customers c on s.cust_id=c.cust_id
+join sh.countries cr on cr.country_id = c.country_id group by country_region;
+-- avg revenue genreated from each region while joining sales,customers and countries tables
+select country_region,avg(amount_sold) as avg_sale from sh.sales s join sh.customers c on s.cust_id=c.cust_id
+join sh.countries cr on cr.country_id=c.country_id group by country_region;
+-- 13. Count the number of sales per month.  
+-- 14. Find the total revenue per month.  
+--It gives which product getting higest revenue
+select prod_id,sum(amount_sold) Higest_revenue from sh.sales group by prod_id
+order by Higest_revenue desc fetch first 1 row only;
+ -- it return which product happens more slaes
+ select prod_id,count(*) as prod_count from sh.sales group by prod_id
+order by prod_count desc  fetch first 1 row only;
+-- It returns which customer spending more amount
+select cust_id,sum(amount_sold) as total_spending_amount from sh.sales group by cust_id order by total_spending_amount desc
+fetch first 1 row only
+-- 18. Find the total revenue per sales channel (e.g., online vs. offline).  
+-- 19. Count the number of sales for each payment method.  
+-- 20. Find the total revenue generated per payment method.  
